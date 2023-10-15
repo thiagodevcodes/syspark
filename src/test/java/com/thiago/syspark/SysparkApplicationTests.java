f6fb6c682046e4975a200924291d11fc6213b43d
thiagodevcodes <euthiagosrodrigues@gmail.com>
2023-10-15 14:18:25 -0300

    Initial Commit

---
+++ src/test/java/com/thiago/syspark/SysparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.syspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
