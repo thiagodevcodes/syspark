af3f461ad4d706c7ff589bd414ab4966ea45a859
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-20 17:00:10 -0300

    feat: Init Backend and add Swagger

---
+++ backend/src/main/java/com/park/syspark/SysparkApplication.java
@@ -0,0 +1,13 @@
+package com.park.syspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysparkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/park/syspark/SysparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.syspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
